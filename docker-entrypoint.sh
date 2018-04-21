48f3dc59c5cc14f1bd6d96e2eff5f2b4fc5de6bb
Louis Laugesen <louis.laugesen@gmail.com>
2018-04-22 02:52:37 +1000

    Export env vars for WordPress access in php
    Fixes issue where site url wasn't being set.
    
    Entrypoint now exposes env vars properly to supervisord and thus
    WordPress.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -e
 
 # Nginx subs, VIRTUAL_HOST is an override for nginx-proxy development setups
-SERVER_PROTO=${SERVER_PROTO:-http}
-SERVER_NAME=${VIRTUAL_HOST:-${SERVER_NAME:-localhost}}
-SERVER_ALIAS=${SERVER_ALIAS:-''}
+export SERVER_PROTO=${SERVER_PROTO:-http}
+export SERVER_NAME=${VIRTUAL_HOST:-${SERVER_NAME:-localhost}}
+export SERVER_ALIAS=${SERVER_ALIAS:-''}
 
 envsubst '$SERVER_NAME $SERVER_ALIAS' < /nginx.conf.template > /etc/nginx/nginx.conf
 
 # WordPress subs
-WP_SITE_URL=${WP_SITE_URL:-$SERVER_PROTO://$SERVER_NAME}
-WP_HOME=${WP_HOME:-$WP_SITE_URL}
+export WP_SITE_URL=${WP_SITE_URL:-"$SERVER_PROTO://$SERVER_NAME"}
+export WP_HOME=${WP_HOME:-$WP_SITE_URL}
 
 supervisord -c /supervisord.conf
